3afde1f9de3d11d816506ac48222b2f436829761
h4xxel <isaksson.axel@telia.com>
2012-12-01 17:56:36 +0100

    added some html

--- status.c
+++ status.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
+char title[]="Status";
+char body[]="<img src=\"graph\" alt=\"graph\" />\n";
+char *html[]={
+	"<!DOCTYPE html>",
+	"<html>",
+	"	<head>",
+	"		<title>", title, "</title>",
+	"	</head>",
+	"	<body>",
+	body,
+	"	</body>",
+	"</html>",
+	NULL,
+};
+
 int main(int argc, char **argv) {
-	printf("Content-type: text/html\nStatus: 200 OK\n\n");
-	printf("CGI-test :D\n");
-	printf("<img src=\"graph\" alt=\"graph\" />\n");
+	printf("Content-type: text/html; charset=utf-8\nStatus: 200 OK\n\n");
+	int i;
+	for(i=0; html[i]; i++)
+		printf("%s\n", html[i]);
 	return 0;
 }
