16378c3027a9593b5a2195c2e6395fcfd68b583b
h4xxel <isaksson.axel@telia.com>
2012-12-01 15:27:25 +0100

    status shows graph

--- status.c
+++ status.c
@@ -3,5 +3,6 @@
 int main(int argc, char **argv) {
 	printf("Content-type: text/html\nStatus: 200 OK\n\n");
 	printf("CGI-test :D\n");
+	printf("<img src=\"graph\" alt=\"graph\" />\n");
 	return 0;
 }
