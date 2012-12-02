6429084abe79469844c173914858d3a6a3094286
Axel Isaksson <isaksson.axel@telia.com>
2012-12-02 02:14:41 +0100

    lan status

--- status.c
+++ status.c
@@ -5,11 +5,17 @@
 #include <net/if.h>
 #include <arpa/inet.h>
 
+static struct {
+	struct ifreq ifreqs[32];
+	int num;
+} interfaces;
+
 char title[]="Status";
 char style[]="/style.css";
 char body[]=
 	"<h1>Status</h1>"
-	"<h2>Network graph</h2>"
+	"<h2>System</h2>"
+	"<h2>3G</h2>"
 	"<img src=\"graph\" alt=\"graph\" />"
 	"";
 char *html[]={
@@ -27,30 +33,50 @@ char *html[]={
 };
 
 void ifconf() {
-	static struct ifreq ifreqs[32];
 	struct ifconf ifconf;
 	int sock;
 	int i;
 	memset(&ifconf, 0, sizeof(ifconf));
-	ifconf.ifc_req=ifreqs;
-	ifconf.ifc_len=sizeof(ifreqs)-1;
+	ifconf.ifc_req=interfaces.ifreqs;
+	ifconf.ifc_len=sizeof(interfaces.ifreqs)-1;
 	if((sock=socket(PF_INET, SOCK_STREAM, 0))<0)
 		return;
 	if(ioctl(sock, SIOCGIFCONF, (char *) &ifconf))
 		return;
-	for(i=0; i<ifconf.ifc_len/sizeof(struct ifreq); i++) {
-		if(ioctl(sock, SIOCGIFFLAGS, (char *) &ifreqs[i]))
+	interfaces.num=ifconf.ifc_len/sizeof(struct ifreq);
+	for(i=0; i<interfaces.num; i++) {
+		if(ioctl(sock, SIOCGIFFLAGS, (char *) &interfaces.ifreqs[i]))
 			continue;
-		printf("interface %s is %s, address %s\n", ifreqs[i].ifr_name, ifreqs[i].ifr_flags&IFF_UP?"up":"down", inet_ntoa(((struct sockaddr_in *) &ifreqs[i].ifr_addr)->sin_addr));
+		//printf("interface %s is %s, address %s\n", ifreqs[i].ifr_name, ifreqs[i].ifr_flags&IFF_UP?"up":"down", inet_ntoa(((struct sockaddr_in *) &ifreqs[i].ifr_addr)->sin_addr));
 	}
 	close(sock);
+}
+
+struct ifreq* ifstatus(char *interface) {
+	int i;
+	for(i=0; i<interfaces.num; i++) {
+		if(!strcmp(interfaces.ifreqs[i].ifr_name, interface);
+			return &interfaces.ifreqs[i];
+	return NULL;
+}
+
+void status_lan() {
+	struct ifreq eth0=ifstatus("eth0");
+	puts("<h2>LAN</h2>");
+	if(eth0&&(eth.ifr_flags&IFF_UP)) {
+		puts("<p>LAN is connected</p>");
+	} else {
+		puts("<p>LAN is disconnected</p>");
+	}
+	puts("<br />");
 }
 
 int main(int argc, char **argv) {
 	printf("Content-type: text/html; charset=utf-8\nStatus: 200 OK\n\n");
 	int i;
+	ifconf();
 	for(i=0; html[i]; i++)
 		printf("%s", html[i]);
-	
+	status_lan();
 	return 0;
 }
